6ba1dda9375e4c0443578a067554cd099c122fc7
huyennt17 <nthuyen.bmth@gmail.com>
2020-08-31 13:22:23 +0200

    Add data+eval+utils folders

---
+++ eval/compute_rouge.py
@@ -0,0 +1,27 @@
+from rouge import Rouge
+
+def compute_rouge_score(can, ref, is_input_files = False):
+    """
+    Compute ROUGE scores
+    :param can: predicted summary
+    :param ref: reference summary
+    :param is_file (bool): whether the inputs are stored in a file
+    :return: dict: ROUGE score
+    """
+    if is_input_files:
+        candidates = [line.strip() for line in open(can, encoding="utf-8")]
+        references = [line.strip() for line in open(ref, encoding="utf-8")]
+    else:
+        candidates = can
+        references = ref
+
+    print("#candidates: {}".format(len(candidates)))
+    print("#references: {}".format(len(references)))
+
+    evaluator = Rouge(
+        metrics = ['rouge-n', 'rouge-l'], max_n = 2, limit_length = False, apply_avg = True
+    )
+
+    scores = evaluator.get_scores(candidates, [[iter] for iter in references])
+
+    return scores
